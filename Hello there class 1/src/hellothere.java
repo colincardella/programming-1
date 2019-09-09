e11d3069e9f89e05a7abffe9f228e13340bff56d
colincardella <colincardella@10.0.0.196>
2019-09-09 18:14:43 -0400

    Ready for review `

---
+++ Hello there class 1/src/hellothere.java
@@ -0,0 +1,28 @@
+
+public class hellothere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+	    //Project Name: Hello There 1
+		System.out.println("hello");
+		System.out.println("My top 10 favorite foods are");
+		System.out.println("my #1 food is pasta");
+		System.out.println("then second is pizza");
+		System.out.println("#3 I like salads");
+		System.out.println("#4 is chicken");
+		System.out.println("#5 is mashed potatos");
+		System.out.println("#6 is chciken noodle soup");
+		System.out.println("#7 is bacon");
+		System.out.println("#8 is fruits");
+		System.out.println("#9 is grilled cheese sandwichs");
+		System.out.println("Finale to #10 which is fish");
+		
+		
+		
+		
+	
+	} 
+		
+}
+
